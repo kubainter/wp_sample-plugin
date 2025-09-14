664409e7ce0e76987f40535bff9c01bc48e995ae
Jakub Grzesiak <jakub.grzesiak@jg-webtech.pl>
2025-09-14 23:56:15 +0200

    remove unnecessary dockblocks in main file

--- graduates.php
+++ graduates.php
@@ -18,21 +18,15 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-// Define plugin constants
 define('GRADUATES_VERSION', '1.0.0');
 define('GRADUATES_FILE', __FILE__);
 define('GRADUATES_DIR', plugin_dir_path(__FILE__));
 define('GRADUATES_URL', plugin_dir_url(__FILE__));
 
-// Include the Loader class manually - needed for autoloader setup
 require_once GRADUATES_DIR . 'src/Loader.php';
 
-// Register autoloader
 Graduates\Loader::registerAutoloader();
-
-// Register lifecycle hooks
 Graduates\Loader::registerHooks();
 
-// Initialize the plugin
 $loader = new Graduates\Loader();
 $loader->init();
